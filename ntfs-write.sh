086fcbcfc988851efdd297ed796475b16121e68c
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-05-08 14:47:29 +0530

    checking if the drive is already mounted

--- ntfs-write.sh
+++ ntfs-write.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Find the first NTFS-formatted drive
-DRIVE_NAME=$(diskutil list | grep "Microsoft Basic Data" | head -n 1 | awk '{print $NF}')
-# you might need to change the "Microsoft Basic Data" if it doesn't detect the drive
+# Find the identifier of the NTFS-formatted drive
+DRIVE_ID=$(diskutil list | grep "Microsoft Basic Data" | awk '{print $NF}')
 
 # Check if a valid drive was found
-if [ -z "$DRIVE_NAME" ]; then
+if [ -z "$DRIVE_ID" ]; then
     echo "No NTFS-formatted drive found"
     exit 1
 fi
 
-# Get the drive identifier
-DRIVE_IDENTIFIER=$(diskutil list | grep "$DRIVE_NAME" | awk '{print $NF}')
+# Get the name of the drive
+DRIVE_NAME=$(diskutil info "$DRIVE_ID" | grep "Volume Name" | awk -F': +' '{print $2}')
 
-# Create a mount point for the drive
-sudo mkdir "/Volumes/$DRIVE_NAME"
+# Check if the drive is already mounted
+if mount | grep "/Volumes/$DRIVE_NAME" > /dev/null; then
+    echo "Drive is already mounted"
+else
+    # Create a mount point for the drive
+    sudo mkdir "/Volumes/$DRIVE_NAME"
 
-# Add the drive to /etc/fstab to enable writing
-echo "LABEL=$DRIVE_NAME none ntfs rw,auto,nobrowse" | sudo tee -a /etc/fstab
+    # Add the drive to /etc/fstab to enable writing
+    echo "LABEL=$DRIVE_NAME none ntfs rw,auto,nobrowse" | sudo tee -a /etc/fstab
 
-# Mount the drive
-sudo mount -t ntfs -o rw,auto,nobrowse "$DRIVE_IDENTIFIER" "/Volumes/$DRIVE_NAME"
+    # Mount the drive
+    sudo mount -t ntfs -o rw,auto,nobrowse "/dev/$DRIVE_ID" "/Volumes/$DRIVE_NAME"
+
+    echo "Drive mounted at /Volumes/$DRIVE_NAME"
+fi
 
 # Open the drive in Finder
 open "/Volumes/$DRIVE_NAME"
